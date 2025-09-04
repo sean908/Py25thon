aac193a99b7c0d44e60e0a7535fbc41e02aa4f1f
sean908 <4495319+sean908@users.noreply.github.com>
2025-09-04 03:28:52 +0000

    - 使用 string 模块定义字符集   - 用 random.choices(k=n) 直接选择 n 个元素   - 消除了 while 循环和计数变量

--- day-05/randomPasswdGenerator.py
+++ day-05/randomPasswdGenerator.py
@@ -1,12 +1,8 @@
 import random
+import string
 
-letters = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '@', '^', '_', '=']
 
 print("Welcome to the PyPassword Generator!")
@@ -15,16 +11,9 @@ nr_symbols = int(input("How many symbols you need in your password? \n"))
 nr_numbers = int(input("How many numbers you need in your password? \n"))
 
 raw_ele = []
-nl = ns = nn = 0
-while nl < nr_letters:
-    raw_ele.append(letters[random.randint(0, len(letters) - 1)])
-    nl += 1
-while ns < nr_symbols:
-    raw_ele.append(symbols[random.randint(0, len(symbols) - 1)])
-    ns += 1
-while nn < nr_numbers:
-    raw_ele.append(numbers[random.randint(0, len(numbers) - 1)])
-    nn += 1
+raw_ele.extend(random.choices(letters, k=nr_letters))
+raw_ele.extend(random.choices(symbols, k=nr_symbols))
+raw_ele.extend(random.choices(numbers, k=nr_numbers))
 
 random.shuffle(raw_ele)
 password = ''.join(raw_ele)
