6f29210d254a77d8ce133e12764cf85db9824992
sean908 <4495319+sean908@users.noreply.github.com>
2025-09-12 09:29:51 +0000

    计算器小程序(没啥技术含量直接提了, README没必要)

---
+++ day-10-funcsWithOutputs/calculator.py
@@ -0,0 +1,65 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def calculator():
+    print(logo)
+    
+    num1 = float(input("What's the first number?: "))
+    
+    while True:
+        for symbol in operations:
+            print(symbol)
+        
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        should_continue = input(f"Type 'y' to continue calculating with {answer}, type 'n' to exit, or type 'r' to restart: ").lower()
+        
+        if should_continue == "y":
+            num1 = answer
+        elif should_continue == "n":
+            print("Goodbye!")
+            break
+        elif should_continue == "r":
+            calculator()
+            break
+
+calculator()
